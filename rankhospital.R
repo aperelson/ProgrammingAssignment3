c344c844d8e33736fc6f46acc0cbf7da1758a72e
Andrew Perelson <Andrew Perelson>
2016-10-17 20:23:48 +0800

    v1 of rankhosp

--- rankhospital.R
+++ rankhospital.R
@@ -1,4 +1,5 @@
-best <- function(state, outcome) { 
+rankhospital <- function(state, outcome, num = "best") { 
+    
     ## Read outcome data
     outcomeofcare <- read.csv("outcome-of-care-measures.csv", colClasses = "character") 
     
@@ -6,40 +7,36 @@ best <- function(state, outcome) {
     if (state %in% unique(outcomeofcare$State) == FALSE) {
         stop("invalid state")
     }
-
+    
     if (is.element(outcome, c('heart attack', 'heart failure', 'pneumonia')) == FALSE) {
         stop("invalid condition")
     }
     
-    ## Return hospital name in that state with lowest 30-day death rate
+    
+    ## Return hospital name in that state with the given rank 30-day death rate
     
     ## Get all the hospitals in the state:
     hospinstate <- outcomeofcare[outcomeofcare$State==state,]
     
     ## Choose which criteria to get the lowest of:
     if (outcome == 'heart attack') {
-        sortingcolumn <- 
-            grep("Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack", 
-                 colnames(hospinstate))
+        sortingcolumn <- 11
     }
     else if (outcome == 'heart failure') {
-        sortingcolumn <- 
-        grep("Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure", 
-             colnames(hospinstate))
+        sortingcolumn <- 17
     }
     else if (outcome == 'pneumonia') {
-        sortingcolumn <- 
-        grep("Lower.Mortality.Estimate...Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia", 
-             colnames(hospinstate))
+        sortingcolumn <- 23
     }
     
     namecolumn <- grep("Hospital.Name", colnames(hospinstate))
-
+    
     #Convert column to numbers for sorting and suppress warnings:
     hospinstate[sortingcolumn] = 
         suppressWarnings(as.numeric(hospinstate[sortingcolumn][hospinstate[sortingcolumn] != 'No']))
     
     #Order by both the condition and the hospital name:    
-    topone <- head(hospinstate[order(hospinstate[sortingcolumn],hospinstate[namecolumn]),], 1)
-    topone$Hospital.Name
+    topone <- hospinstate[order(hospinstate[sortingcolumn],hospinstate[namecolumn]),]
+    topone$Hospital.Name[num]
+            
 }
